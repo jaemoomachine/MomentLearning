01b9ee9a4473af194fee4cc8b932b79d04efef80
JaemooMachine <jaemoomachine@gmail.com>
2025-08-15 19:19:35 +0900

    Update TE_MomentFFN.sh

--- scripts/TE_MomentFFN.sh
+++ scripts/TE_MomentFFN.sh
@@ -1,7 +1,5 @@
-python -u run.py --model MomentTE --ffn_type moment_ffn --strategy beta --data_name fBM --L_sub 2 --K 4 gpu 0
-
-python -u run.py --model MomentTE --ffn_type moment_ffn --strategy beta --data_name Levy --L_sub 2 --K 4 gpu 0
-
+python -u run.py --model MomentTE --ffn_type moment_ffn --strategy beta --data_name fBM --L_sub 2 --K 4 gpu 0 &
+python -u run.py --model MomentTE --ffn_type moment_ffn --strategy beta --data_name Levy --L_sub 2 --K 4 gpu 0 &
 python -u run.py --model MomentTE --ffn_type moment_ffn --strategy beta --data_name fBM_Levy --L_sub 2 --K 4 gpu 0
 
 
